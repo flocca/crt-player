2ad95e7426330bb4bcf387e9a61fd43b600a02cf
Giacomo Antolini <giacomo.antolini@gmail.com>
2026-04-13 16:55:01 +0200

    feat: project setup with config and dependencies

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+CHROMECAST_NAME = os.environ.get("CRT_CHROMECAST_NAME", "Living Room TV")
+MAX_VIDEO_HEIGHT = int(os.environ.get("CRT_MAX_VIDEO_HEIGHT", "576"))
+TEMP_DIR = os.environ.get("CRT_TEMP_DIR", "/tmp/crt_cast")
+FILE_TTL_HOURS = int(os.environ.get("CRT_FILE_TTL_HOURS", "24"))
+SERVER_PORT = int(os.environ.get("CRT_SERVER_PORT", "8765"))
